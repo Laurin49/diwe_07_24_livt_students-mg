7463d7a4792bf47f9277d5b232e54d2c0bcb4275
Laurin49 <welter@diwe.info>
2024-06-29 14:52:20 +0200

    dev01 Models, Migrations, Factories und Seeders o.k.

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $users = [
+            [
+                'name' => 'Admin',
+                'email' => 'admin@hsv.de',
+                'password' => bcrypt('hsv1887tv'),
+            ],
+        ];
+
+        User::insert($users);
+    }
+}
