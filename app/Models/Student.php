fca4764c51661576564295bb9e651dc3cad0c96e
Laurin49 <welter@diwe.info>
2024-07-01 13:07:19 +0200

    dev02 Displaying Students Data

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -11,6 +11,8 @@ class Student extends Model
     use HasFactory;
     protected $fillable = ['name', 'email', 'class_id', 'section_id'];
 
+    protected $with = ['class', 'section'];
+
     public function class(): BelongsTo {
         return $this->belongsTo(Classes::class, 'class_id');
     }
